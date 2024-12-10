c8c7ecfcf32675c5f2a86574ea4243036cbf2745
“hyunwook-sevo” <seojy1276@naver.com>
2024-12-10 23:15:33 +0900

    design: 간단한 색상 변경

--- poketmonPhonebook/poketmonPhonebook/CustomTableViewCell.swift
+++ poketmonPhonebook/poketmonPhonebook/CustomTableViewCell.swift
@@ -5,4 +5,67 @@
 //  Created by t2023-m0149 on 12/10/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class CustomTableViewCell: UITableViewCell {
+    private let profileImageView = UIImageView()
+    private let nameLabel = UILabel()
+    private let phoneLabel = UILabel()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUIViews()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupUIViews() {
+        contentView.addSubview(profileImageView)
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(phoneLabel)
+
+        // 프로필 이미지 설정
+        profileImageView.contentMode = .scaleAspectFill
+        profileImageView.layer.cornerRadius = 30
+        profileImageView.clipsToBounds = true
+        profileImageView.layer.borderWidth = 2
+        profileImageView.layer.borderColor = UIColor.lightGray.cgColor
+        profileImageView.backgroundColor = .white
+
+        // 이름 라벨
+        nameLabel.font = .boldSystemFont(ofSize: 16)
+        nameLabel.textColor = .black
+
+        // 전화번호 라벨
+        phoneLabel.font = .systemFont(ofSize: 14)
+        phoneLabel.textColor = .black
+        phoneLabel.textAlignment = .right
+
+        // 레이아웃 설정
+        profileImageView.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(45)
+            $0.centerY.equalToSuperview()
+            $0.width.height.equalTo(60)
+        }
+
+        nameLabel.snp.makeConstraints {
+            $0.leading.equalTo(profileImageView.snp.trailing).offset(30)
+            $0.top.equalToSuperview().offset(30)
+            $0.trailing.lessThanOrEqualTo(phoneLabel.snp.leading).offset(-8)
+        }
+
+        phoneLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview().offset(-45)
+            $0.centerY.equalTo(nameLabel.snp.centerY)
+            $0.width.equalTo(120)
+        }
+    }
+
+    func configure(with contact: Contact) {
+        nameLabel.text = contact.name
+        phoneLabel.text = contact.phoneNumber
+    }
+}
--- poketmonPhonebook/poketmonPhonebook/ViewController.swift
+++ poketmonPhonebook/poketmonPhonebook/ViewController.swift
@@ -6,14 +6,87 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    private let tableView = UITableView()
+    private let addButton = UIButton()
+    private var contacts: [Contact] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        setupNavigationBar()
+        setupTableView()
+        loadDummyData()
     }
 
+    private func setupNavigationBar() {
+        // 제목 설정
+        navigationItem.title = "친구 목록"
+        navigationController?.navigationBar.titleTextAttributes = [.font: UIFont.boldSystemFont(ofSize: 18)]
 
+        // 추가 버튼 설정
+        addButton.setTitle("추가", for: .normal)
+        addButton.setTitleColor(.lightGray, for: .normal)
+        addButton.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        addButton.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
+
+        let rightBarButtonItem = UIBarButtonItem(customView: addButton)
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+
+    private func setupTableView() {
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: "CustomCell")
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+
+        // 테이블뷰 제약조건 설정
+        tableView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(40)
+            $0.bottom.equalToSuperview()
+        }
+    }
+
+    private func loadDummyData() {
+        contacts = [
+            Contact(name: "name", phoneNumber: "010-0000-0000", profileImage: nil),
+            Contact(name: "name", phoneNumber: "010-0000-0000", profileImage: nil),
+            Contact(name: "name", phoneNumber: "010-0000-0000", profileImage: nil),
+            Contact(name: "name", phoneNumber: "010-0000-0000", profileImage: nil),
+            Contact(name: "name", phoneNumber: "010-0000-0000", profileImage: nil),
+            Contact(name: "name", phoneNumber: "010-0000-0000", profileImage: nil)
+        ]
+        tableView.reloadData()
+    }
+
+//    @objc private func addButtonTapped() {
+//        print("추가 버튼이 눌렸습니다.")
+//    }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as? CustomTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.configure(with: contacts[indexPath.row])
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    @objc private func addButtonTapped() {
+        let phoneBookVC = PhoneBookViewController()
+        navigationController?.pushViewController(phoneBookVC, animated: true)
+    }
+}
